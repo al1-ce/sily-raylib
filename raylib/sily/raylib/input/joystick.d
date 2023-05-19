32a21b6024a93c1d24a4fc00e5a208c575c8feb6
al1-ce <al1-ce@list.ru>
2023-05-19 14:58:51 +0300

    SDL joy

--- raylib/sily/raylib/input/joystick.d
+++ raylib/sily/raylib/input/joystick.d
@@ -22,3 +22,15 @@ alias joyAxis = gamepadAxis;
 
 /// Get last button pressed
 alias joystickLastButton = gamepadLastButton;
+
+// SDL joy mappings
+// string map = "" ~
+//     "leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a4,righttrigger:a5" ~
+//     "dpup:b0,dpright:b1,dpdown:b2,dpleft:b3,y:b4,b:b5,a:b6,x:b7," ~
+//     "leftshoulder:b8,misc1:b9,rightshoulder:b10," ~
+//     "touchpad:b11,back:b12,guide:b13,start:b14,leftstick:b15,rightstick:b16" ~
+//     ",platform:Windows,";
+
+// joystickSetMappings("030000001d2300000102000000000000, VKBsim Gladiator EVO  L  ," ~ map);
+// joystickSetMappings("030000001d2300000002000000000000, VKBsim Gladiator EVO  R  ," ~ map);
+
